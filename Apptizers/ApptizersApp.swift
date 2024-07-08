499f97ff3a230496d05a16d0afe87b1af3dfd5cd
Tauan Camargo <lukyhenson@gmail.com>
2024-07-08 19:18:06 -0300

    Initial Commit

---
+++ Apptizers/ApptizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApptizersApp.swift
+//  Apptizers
+//
+//  Created by Tauan Tathiell Camargo on 08/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ApptizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apptizers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Apptizers
+//
+//  Created by Tauan Tathiell Camargo on 08/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
